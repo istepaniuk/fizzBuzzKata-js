43b3bf135ee34072852f7cf0ea6a6e9f5b1747f8
istepaniuk <istepaniuk@localhost>
2013-01-27 21:52:56 +0100

    green

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
     "use  strict";
 
     function fizzBuzz(input){
+        if (isDivisible(input, 5) && isDivisible(input, 3))
+            return "FizzBuzz";
         if (isDivisible(input, 5))
             return "Buzz";
         if (isDivisible(input, 3)) 
