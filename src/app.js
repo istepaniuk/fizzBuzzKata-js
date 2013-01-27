ee11c2e4b33684b46c8b9fa4376c05e1834dd973
istepaniuk <istepaniuk@localhost>
2013-01-27 22:59:07 +0100

    green

--- src/app.js
+++ src/app.js
@@ -4,8 +4,12 @@ window.fb = window.fb || {};
     "use  strict";
     
     function Sequence(qty){
-        this.generate = function(){
-            return [1];
+        this.generate = function(qty){
+            result = [];
+            for(var i = 1; i < qty+1; i++){
+                result.push(i);
+            }
+            return result;
         };
     }
 
