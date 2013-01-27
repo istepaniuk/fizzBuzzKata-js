332151cdaefb2c249eebd41564eada8725852b92
istepaniuk <istepaniuk@localhost>
2013-01-27 23:54:12 +0100

    red, tells a widget to print the generated sequence

--- test/unit/interactorSpecs.js
+++ test/unit/interactorSpecs.js
@@ -4,11 +4,27 @@ describe("FizzBuz interactor", function(){
     it("asks for a sequence of 100 numbers", function(){
         interactor = new fb.FizzBuzzInteractor();
         var seqGenerator = new fb.Sequence();      
+        var listWidget = { showList: function(){} };
         interactor.attachSequenceGenerator(seqGenerator);
+        interactor.attachNumberListWidget(listWidget);
         spyOn(seqGenerator,"generate");
        
         interactor.start();
 
         expect(seqGenerator.generate).toHaveBeenCalledWith(100);
+    });
+
+    it("tells a widget to print the generated sequence", function(){
+        var list = ["xxx", "yyy"];
+        interactor = new fb.FizzBuzzInteractor();
+        var seqGenerator = { generate: function(){return list} };
+        var listWidget = { showList: function(){} };
+        interactor.attachSequenceGenerator(seqGenerator);
+        interactor.attachNumberListWidget(listWidget);
+        spyOn(listWidget,"showList");
+
+        interactor.start();
+
+        expect(listWidget.showList).toHaveBeenCalledWith(list);
     });
 });
