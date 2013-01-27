a4eccbd53e5fc9a9af5a51460007966db1f99d98
istepaniuk <istepaniuk@localhost>
2013-01-27 22:26:42 +0100

    extracted rules.js with all the FizzBuzz rules

--- src/app.js
+++ src/app.js
@@ -1,50 +1,13 @@
 (function(window, undefined) {
     "use  strict";
     
-    var FIZZ = "Fizz",
-        BUZZ = "Buzz",
-        FIZZ_BUZZ = "FizzBuzz";
-
-    var FIZZ_DIVS = [3],
-        BUZZ_DIVS = [5],
-        FIZZ_BUZZ_DIVS = [3,5];
-
-    var orderedRules = {
-        fizzBuzz: { 
-            returns: FIZZ_BUZZ, 
-            condition: divisibleByListCondition(FIZZ_BUZZ_DIVS) },
-        fizz: { 
-            returns: FIZZ, 
-            condition: divisibleByListCondition(FIZZ_DIVS) },
-        buzz: { 
-            returns: BUZZ, 
-            condition: divisibleByListCondition(BUZZ_DIVS) },
-        };
-    
     function fizzBuzz(input){
-        for(var i in orderedRules){
-            if(orderedRules[i].condition(input))
-                return orderedRules[i].returns;
+        var rules = fb.fizzBuzzRules;
+        for(var i in rules){
+            if(rules[i].condition(input))
+                return rules[i].returns;
         }
         return input;
-    }
-
-    function divisibleByListCondition(divisors){
-        return function(x) {
-            return isDivisibleByList(x, divisors);
-        };
-    }
-
-    function isDivisibleByList(x, divisorList){
-        for(var i = 0; i < divisorList.length; i++){
-            if(isNotDivisible(x, divisorList[i])) 
-                return false;
-        }
-        return true;
-    }
-
-    function isNotDivisible(x, by){
-        return x % by != 0;
     }
 
     window.fb = window.fb || {};
---
+++ src/rules.js
@@ -0,0 +1,44 @@
+(function(window, undefined) {
+    "use  strict";
+    
+    var FIZZ = "Fizz",
+        BUZZ = "Buzz",
+        FIZZ_BUZZ = "FizzBuzz";
+
+    var FIZZ_DIVS = [3],
+        BUZZ_DIVS = [5],
+        FIZZ_BUZZ_DIVS = [3,5];
+
+    var orderedRules = {
+        fizzBuzz: { 
+            returns: FIZZ_BUZZ, 
+            condition: divisibleByListCondition(FIZZ_BUZZ_DIVS) },
+        fizz: { 
+            returns: FIZZ, 
+            condition: divisibleByListCondition(FIZZ_DIVS) },
+        buzz: { 
+            returns: BUZZ, 
+            condition: divisibleByListCondition(BUZZ_DIVS) },
+        };
+    
+    function divisibleByListCondition(divisors){
+        return function(x) {
+            return isDivisibleByList(x, divisors);
+        };
+    }
+
+    function isDivisibleByList(x, divisorList){
+        for(var i = 0; i < divisorList.length; i++){
+            if(isNotDivisible(x, divisorList[i])) 
+                return false;
+        }
+        return true;
+    }
+
+    function isNotDivisible(x, by){
+        return x % by != 0;
+    }
+
+    window.fb = window.fb || {};
+    window.fb.fizzBuzzRules = orderedRules;
+}(window))
