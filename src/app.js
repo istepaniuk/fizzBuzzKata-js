9125c389f510db2f83ea8219d9ee56bd92849bd1
istepaniuk <istepaniuk@localhost>
2013-01-27 22:01:48 +0100

    refactor, changed isDivisible to accept a list of divisors for wich x is divisible

--- src/app.js
+++ src/app.js
@@ -5,21 +5,26 @@
         BUZZ = "Buzz",
         FIZZ_BUZZ = "FizzBuzz";
 
-    var FIZZ_DIV = 3,
-        BUZZ_DIV = 5;
+    var FIZZ_DIVS = [3],
+        BUZZ_DIVS = [5],
+        FIZZ_BUZZ_DIVS = [3,5];
     
     function fizzBuzz(input){
-        if (isDivisible(input, BUZZ_DIV) && isDivisible(input, FIZZ_DIV))
+        if (isDivisible(input, FIZZ_BUZZ_DIVS))
             return FIZZ_BUZZ;
-        if (isDivisible(input, BUZZ_DIV))
+        if (isDivisible(input, BUZZ_DIVS))
             return BUZZ;
-        if (isDivisible(input, FIZZ_DIV)) 
+        if (isDivisible(input, FIZZ_DIVS)) 
             return FIZZ;
         return input;
     }
 
-    function isDivisible(x, by){
-        return x % by == 0;
+    function isDivisible(x, byList){
+        for(var i = 0; i < byList.length; i++){
+            if(x % byList[i] != 0) 
+                return false;
+        }
+        return true;
     }
     
     window.fb = window.fb || {};
