0e99c6ad66ac53fb57e47a21affeb6df3285b6ff
istepaniuk <istepaniuk@localhost>
2013-01-27 22:03:18 +0100

    refactor, rename

--- src/app.js
+++ src/app.js
@@ -10,23 +10,23 @@
         FIZZ_BUZZ_DIVS = [3,5];
     
     function fizzBuzz(input){
-        if (isDivisible(input, FIZZ_BUZZ_DIVS))
+        if (isDivisibleByList(input, FIZZ_BUZZ_DIVS))
             return FIZZ_BUZZ;
-        if (isDivisible(input, BUZZ_DIVS))
+        if (isDivisibleByList(input, BUZZ_DIVS))
             return BUZZ;
-        if (isDivisible(input, FIZZ_DIVS)) 
+        if (isDivisibleByList(input, FIZZ_DIVS)) 
             return FIZZ;
         return input;
     }
 
-    function isDivisible(x, byList){
-        for(var i = 0; i < byList.length; i++){
-            if(x % byList[i] != 0) 
+    function isDivisibleByList(x, divisorList){
+        for(var i = 0; i < divisorList.length; i++){
+            if(x % divisorList[i] != 0) 
                 return false;
         }
         return true;
     }
-    
+
     window.fb = window.fb || {};
     window.fb.fizzBuzz = fizzBuzz;
 }(window))
