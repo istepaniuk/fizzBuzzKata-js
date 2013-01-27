d1c51a934235b31748471abd2e73de924df6f73f
istepaniuk <istepaniuk@localhost>
2013-01-27 21:43:06 +0100

    refactor, removed magic literals from the tests

--- test/unit/specs.js
+++ test/unit/specs.js
@@ -1,24 +1,27 @@
 describe("FizzBuzz", function() {
+    var FIZZ = "Fizz",
+        BUZZ = "Buzz";
+
 	it("returns 1 for 1",function(){
 		expect(fb.fizzBuzz(1)).toEqual(1);
 	});
 
     it("returns Fizz for 3", function(){
-        expect(fb.fizzBuzz(3)).toEqual("Fizz");
+        expect(fb.fizzBuzz(3)).toEqual(FIZZ);
     });
 
     it("returns Buzz for 5", function(){
-        expect(fb.fizzBuzz(5)).toEqual("Buzz");
+        expect(fb.fizzBuzz(5)).toEqual(BUZZ);
     });
 
     it("returns Fizz for other numbers divisible by 3", function(){
-        expect(fb.fizzBuzz(6)).toEqual("Fizz");
-        expect(fb.fizzBuzz(9)).toEqual("Fizz");
+        expect(fb.fizzBuzz(6)).toEqual(FIZZ);
+        expect(fb.fizzBuzz(9)).toEqual(FIZZ);
     });
 
     it("returns Buzz for other numbers divisible by 5", function(){
-        expect(fb.fizzBuzz(20)).toEqual("Buzz");
-        expect(fb.fizzBuzz(10)).toEqual("Buzz");
+        expect(fb.fizzBuzz(20)).toEqual(BUZZ);
+        expect(fb.fizzBuzz(10)).toEqual(BUZZ);
     });
 
     it("returns the input for other numbers not divisible by either 5 or 3", function(){
