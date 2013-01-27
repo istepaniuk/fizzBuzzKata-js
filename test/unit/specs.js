0992c0f6c70e1cbb82d16d5e92e2bc02a469e721
istepaniuk <istepaniuk@localhost>
2013-01-27 21:24:29 +0100

    red, returns Fizz for 3

--- test/unit/specs.js
+++ test/unit/specs.js
@@ -2,5 +2,9 @@ describe("FizzBuzz", function() {
 	it("returns 1 for 1",function(){
 		expect(fb.fizzBuzz(1)).toEqual(1);
 	});
+
+    it("returns Fizz for 3", function(){
+        expect(fb.fizzBuzz(3)).toEqual("Fizz");
+    })
 });
 
