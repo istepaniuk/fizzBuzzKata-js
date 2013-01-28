ec6642632d7e7414dfbc42908bb6906049c05a19
istepaniuk <istepaniuk@localhost>
2013-01-28 08:40:34 +0100

    simplifying the spec test, it only tests that the whole thing works end to end.

--- test/spec/appSpecs.js
+++ test/spec/appSpecs.js
@@ -1,22 +1,12 @@
 describe("FizzBuzz Kata application", function() {
-    var $appBody;
-    beforeEach(function(){
-        $appBody = $("#sut-frame").contents().find("body");
-    });
 
-    it("contains a hundred list items", function(){
-        expect($appBody.find("li").length).toBe(100);
-    });
-
-    it("contains some Fizz, Buzz and FizzBuzz list items", function(){
-        expect(containsLiWithText($appBody, 'Buzz')).toBeTruthy();
-        expect(containsLiWithText($appBody, 'Fizz')).toBeTruthy();
-        expect(containsLiWithText($appBody, 'FizzBuzz')).toBeTruthy();
+    it("prints a list of a hundred items", function(){
+        expect(theQuantityOfListItems()).toBe(100);
     });
     
-    function containsLiWithText($element, text){
-       var selector = "li:contains('" + text + "')";
-       return $element.find(selector).length > 0;
+    function theQuantityOfListItems(){
+        var $appBody = $("#sut-frame").contents().find("body");
+        return $appBody.find("li").length;
     }
 });
 
