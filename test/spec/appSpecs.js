2c52956be9508318a1d5b050727a42732a85bd5a
istepaniuk <istepaniuk@localhost>
2013-01-28 01:24:44 +0100

    red, spec, contains some Fizz, Buzz and FizzBuzz list items

--- test/spec/appSpecs.js
+++ test/spec/appSpecs.js
@@ -7,5 +7,16 @@ describe("FizzBuzz Kata application", function() {
     it("contains a hundred list items", function(){
         expect($appBody.find("li").length).toBe(100);
     });
+
+    it("contains some Fizz, Buzz and FizzBuzz list items", function(){
+        expect(containsLiWithText($appBody, 'Buzz')).toBeTruthy();
+        expect(containsLiWithText($appBody, 'Fizz')).toBeTruthy();
+        expect(containsLiWithText($appBody, 'FizzBuzz')).toBeTruthy();
+    });
+    
+    function containsLiWithText($element, text){
+       var selector = "li:contains('" + text + "')";
+       return $element.find(selector).length > 0;
+    }
 });
 
